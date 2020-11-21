dd17ad8ab46ab8d30323bb2e128dc3b8bcf293e3
ShaunakSen <shaunak1105@gmail.com>
2020-11-21 20:38:49 +0530

    read randomized data

--- Time Series Charts/generate_data.py
+++ Time Series Charts/generate_data.py
@@ -2,41 +2,20 @@ import numpy as np
 import pandas as pd
 import pickle
 
-column_meta_data = {
-    'sku_id': {
-        'type': 'string'
-    },
-    'Target': {
-        'type': 'float',
-        'min': 0,
-        'max': 500,
-        'mean': 0,
-        'std_dev': 7
-    },
-    'department': {
-        'type': 'string'
-    },
-    'cal_date': {
-        'type': 'date'
-    },
-    'weeks_ahead': 1
-}
+
+def read_data():
+    """
+    Pick out 10 random skus
+    set max_wa = 5
+    Get data for wa = 1->5 for these skus
+    randomize the sales
+    randomize the sku ids
+    """
+    random_sales_data = pd.read_csv('C:\\Users\\shaun\\Documents\\my_projects\\Data-Visualiation\\Time Series Charts\\data\\random_data.csv')
+
+    random_sales_data.drop(columns=['Unnamed: 0'], inplace=True)
+
+    return random_sales_data
 
 
-data_params = {
-    'num_skus': 100,
-    'FIRST_VALID_DATE': '2020-01-05',
-    'max_weeks_ahead': 5
-
-}
-
-"""
-X axis : fcdate
-y : sales
-say we have forecasted for wa = 1,2,3
-given date d1, we should have d2, d3 d4, for wa = d1+1, d1+2, d1+3 and the sales for that
-Also pred sales for that
-
-"""
-
-print (np.random.normal(loc=100.0, scale=300, size=100))
+random_sales_data = read_data()
