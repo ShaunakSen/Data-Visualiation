30b8f764d62965e6b91777f49e5bf0409a209ae0
ShaunakSen <shaunak1105@gmail.com>
2023-11-11 10:26:38 +0530

    adding callbacks

--- Getting started with Dash/hello world/app.py
+++ Getting started with Dash/hello world/app.py
@@ -1,37 +1,55 @@
-from dash import Dash, html, dash_table, dcc
+# https://dash.plotly.com/tutorial
+from dash import Dash, html, dash_table, dcc, callback, Output, Input
 import pandas as pd
 import plotly.express as px
 
 
 URL = 'https://raw.githubusercontent.com/plotly/datasets/master/gapminder2007.csv'
 
-def generate_bar_plot(df: pd.DataFrame) -> None:
-    fig = px.histogram(data_frame=df, x='continent', y='lifeExp', histfunc='avg')
-    return fig
-
 def read_data(path: str) -> pd.DataFrame:
     return pd.read_csv(path)
 
-
+df = read_data(URL)
 # This line is known as the Dash constructor and is responsible for initializing your app.
-app = Dash(__name__)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
-def setup_app_layout(df: pd.DataFrame):
-    # The app layout represents the app components that will be displayed in the web browser, 
-    # normally contained within a html.Div. In this example, a single component was added: another html.Div.
-    #  The Div has a few properties, such as children, which we use to add text content 
-    # to the page: "Hello World".
-    app.layout = html.Div(
-        children=[
-            html.Div(children="Hello World"),
-            dash_table.DataTable(data=df.to_dict("records"), page_size=10),
-            dcc.Graph(figure=generate_bar_plot(df))
-        ]
-    )
+
+# App layout
+app.layout = html.Div([
+    html.Div(className='row', children='My First App with Data, Graph, and Controls',
+             style={'textAlign': 'center', 'color': 'blue', 'fontSize': 30}),
+
+    html.Div(className='row', children=[
+        dcc.RadioItems(options=['pop', 'lifeExp', 'gdpPercap'],
+                       value='lifeExp',
+                       inline=True,
+                       id='controls-and-radio-item')
+    ]),
+
+    html.Div(className='row', children=[
+        html.Div(className='six columns', children=[
+            dash_table.DataTable(data=df.to_dict('records'), page_size=11, style_table={'overflowX': 'auto'})
+        ]),
+        html.Div(className='six columns', children=[
+            dcc.Graph(figure={}, id='controls-and-graph')
+        ])
+    ])
+])
+# Add controls to build the interaction
+
+
+@callback(
+    Output(component_id="controls-and-graph", component_property="figure"),
+    Input(component_id="controls-and-radio-item", component_property="value")
+)
+def generate_bar_plot(chosen_col: str) -> None:
+    fig = px.histogram(data_frame=df, x='continent', y=chosen_col, histfunc='avg')
+    return fig
+
 
 if __name__ == "__main__":
-    df = read_data(URL)
+    
     host = '127.0.0.1'
     port = 8055
-    setup_app_layout(df)
     app.run(debug=True, host=host, port=port)
